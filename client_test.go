8e2b75a582873b112608c080365c7aa01d3abc76
French Ben <me+git@frenchben.com>
2016-04-04 17:13:08 -0700

    Added todo to test
    Signed-off-by: French Ben <me+git@frenchben.com>

--- client_test.go
+++ client_test.go
@@ -2,6 +2,8 @@ package goketo
 
 import "testing"
 
+//@TODO Implement Unit Test and Integration tests (BATS)
+
 func Testdo(t *testing.T)   {}
 func TestGet(t *testing.T)  {}
 func TestPost(t *testing.T) {}
