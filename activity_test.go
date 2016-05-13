3a073490361f056498bac32bd3018c8ec654a10c
French Ben <me+git@frenchben.com>
2016-05-12 18:29:28 -0700

    initial test
    Signed-off-by: French Ben <me+git@frenchben.com>

---
+++ activity_test.go
@@ -0,0 +1,8 @@
+package goketo
+
+import "testing"
+
+func TestgetPagingToken(t *testing.T)   {}
+func TestGetActivityTypes(t *testing.T) {}
+func TestGetActivity(t *testing.T)      {}
+func TestGetLeadChanges(t *testing.T)   {}
